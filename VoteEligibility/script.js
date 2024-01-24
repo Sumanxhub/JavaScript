6cbb10961223843abb6f23ced90c9b0023f4e6e9
Suman Bhaskar <126346980+Sumanxhub@users.noreply.github.com>
2024-01-24 22:50:00 +0000

    JavaScipt

--- VoteEligibility/script.js
+++
@@ -1,8 +0,0 @@
-function checkage() {
-    let age = document.getElementById("age").value;
-    if (age > 17) {
-       alert("You are eligible to  vote.");
-    } else {
-        alert("Sorry , You are not eligible to vote yet.");
-    }
-}
